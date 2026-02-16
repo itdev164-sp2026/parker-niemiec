dd91eabab13ec9de9979e9afeb3e6301f935be9f
Parker Niemiec <niemiecp@matc.edu>
2026-02-16 11:54:13 -0600

    Completed Activity 3

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,24 +1,30 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from '../components/layout'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from '../components/layout';
 
 const BlogPost = ({ data }) => {
-    const { title } = data.contentfulBlogPost;
+    const { title, body } = data.contentfulBlogPost;
 
     return (
         <Layout>
             <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
-    )
+    );
 }
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-    query ($slug: String!) {
+    query blogPostQuery($slug: String!) {
         contentfulBlogPost(slug: {eq: $slug}) {
             title
             slug
+            body {
+                childMarkdownRemark {
+                    html
+                }
+            }
         }
     }
 `
